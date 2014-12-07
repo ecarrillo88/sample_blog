587e2ffae1696cf11ddbee2a428586203779ae24
Enrique Carrillo <enriquecarrillo88@gmail.com>
2014-12-07 13:40:51 +0100

    Add presence validations in the title and text

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :author, :text, :title
 
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: :true
+  validates :text, presence: :true
 end
