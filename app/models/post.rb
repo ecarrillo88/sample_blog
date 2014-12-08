f42dd7b0baaa7423bf217912981e4651d1969abb
Enrique Carrillo <enriquecarrillo88@gmail.com>
2014-12-08 18:05:31 +0100

    Add user references to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :text, :title
 
+  belongs_to :user
   has_many :comments, dependent: :destroy
 
   validates :title, presence: :true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141206182302) do
+ActiveRecord::Schema.define(:version => 20141208165011) do
 
   create_table "comments", :force => true do |t|
     t.string   "user"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20141206182302) do
     t.string   "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
