31cbbbc61118018b65d3f4012c3b9067e8623841
Enrique Carrillo <enriquecarrillo88@gmail.com>
2014-12-08 18:25:47 +0100

    Update form with reference to the user who created it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :author, :text, :title
+  attr_accessible :title, :user_id, :author, :text
 
   belongs_to :user
   has_many :comments, dependent: :destroy
